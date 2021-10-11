cb3aea78081da68c979c9acb08dee1e79988cdca
Oguz Guzel <44399848+Oguz-Guzel@users.noreply.github.com>
2021-10-11 15:49:28 +0300

    remove files

--- HH/Bamboo/tautaugg_bamboo/runGetFiles.sh
+++
@@ -1,8 +0,0 @@
-. ./getFiles.sh ZGTo2LG_TuneCUETP8M1_14TeV-amcatnloFXFX-pythia8_200PU
-# . ./getFiles.sh                                                                                 
-# . ./getFiles.sh 
-# . ./getFiles.sh 
-#. ./getFiles.sh W3JetsToLNu_TuneCUETP8M1_14TeV-madgraphMLM-pythia8_200PU 
-#. ./getTIFRFiles.sh DiPhotonJetsBox_MGG-80toInf_14TeV-Sherpa_200PU
-#. ./getTIFRFiles.sh GJet_Pt-40toInf_DoubleEMEnriched_MGG-80toInf_TuneCUEP8M2T4_14TeV_Pythia8_200PU
-#. ./getFiles.sh 
